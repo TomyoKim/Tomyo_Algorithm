7d8912b01cd1488276ddabc8856e1e0cacf5f4b3
Tomyo Kim <87464286+TomyoKim@users.noreply.github.com>
2023-01-28 20:51:45 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.py
@@ -0,0 +1,11 @@
+n=int(input())
+first_word=list(input())
+first_word_len=len(first_word)
+
+for i in range(n-1):
+    other_word=list(input())
+    for j in range(first_word_len):
+        if first_word[j]!=other_word[j]:
+            first_word[j]='?'
+            
+print(''.join(first_word))
