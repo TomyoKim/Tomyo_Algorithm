62f2b69f980e50ea0bb66507759f63a61de940bb
Tomyo Kim <87464286+TomyoKim@users.noreply.github.com>
2023-01-30 23:02:23 +0900

    [Gold II] Title: K번째 수, Time: 688 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.py
+++ 백준/Gold/1300. K번째 수/K번째 수.py
@@ -1,17 +1,24 @@
 n = int(input())
 k = int(input())
 
-def binary_search(target, start, end):
-    while start<=end:
-        mid=(start+end)//2
-        
-        cnt=0
+def binary_search(start, end):
+    while start <= end:
+        mid = (start + end)//2
+
+        cnt = 0
+
         for i in range(1, n+1):
-            cnt+=min(mid//i, n)
-        if cnt>=target:
-            end=mid-1
+            cnt += min(mid//i, n)
+            '''m//i는 i*(m//i)
+            i번째 행에 있는 값은 i*j
+            i번째 행의 최댓값은 i*m
+            i번째 행에서 m보다 작은 값으 개수는 m//i 또는 n'''
+
+        if cnt >= k:
+            end = mid-1
         else:
-            start=mid+1
+            start = mid+1
+
     return start
 
-print(binary_search(k, 1, n*n))
+print(binary_search(1, k))
