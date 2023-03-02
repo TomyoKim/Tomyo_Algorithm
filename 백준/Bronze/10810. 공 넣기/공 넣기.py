5730e088ffa4d26fcb6a624a39b6227814eb9387
Tomyo Kim <87464286+TomyoKim@users.noreply.github.com>
2023-03-02 13:40:40 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+b = [0]*(n+1)
+for _ in range(m):
+    i, j, k = map(int, input().split())
+    for t in range(i, j+1):
+        b[t] = k
+
+for i in range(1, n+1):
+    print(b[i], end=' ')
