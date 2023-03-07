2cb47ecc39a2c2b8b0bf984139ff0d610241d345
Tomyo Kim <87464286+TomyoKim@users.noreply.github.com>
2023-03-07 17:43:19 +0900

    [Silver III] Title: 회전하는 큐, Time: 64 ms, Memory: 34128 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+n, m = map(int, input().split())
+position = list(map(int, input().split()))
+dq = deque([i for i in range(1, n+1)])
+
+ans = 0
+
+for i in position:
+    while True:
+        if dq[0] == i:
+            dq.popleft()
+            break
+        else:
+            if dq.index(i) < len(dq)/2:
+                while dq[0] != i:
+                    dq.append(dq.popleft())
+                    ans += 1
+            else:
+                while dq[0] != i:
+                    dq.appendleft(dq.pop())
+                    ans += 1
+
+print(ans)
