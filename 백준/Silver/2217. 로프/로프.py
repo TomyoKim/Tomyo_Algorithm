7c19f55b01d1f5c066e9225c49cb9eb4fd7d16e6
Tomyo Kim <87464286+TomyoKim@users.noreply.github.com>
2023-04-21 15:22:54 +0900

    [Silver IV] Title: 로프, Time: 4016 ms, Memory: 39268 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,15 @@
+n = int(input())
+m = []
+
+for _ in range(n):
+    m.append(int(input()))
+
+m.sort(reverse=True)
+
+result = []
+
+for i in range(n):
+    result.append(m[i]*(i+1))
+
+
+print(max(result))
