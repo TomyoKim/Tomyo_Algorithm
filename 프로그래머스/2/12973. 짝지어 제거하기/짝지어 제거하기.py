3135e6e967c8dfae90588abaedcea054870343c2
Tomyo Kim <87464286+TomyoKim@users.noreply.github.com>
2023-11-16 16:21:41 +0900

    [level 2] Title: 짝지어 제거하기, Time: 132.97 ms, Memory: 14.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,13 @@
+def solution(s):
+    answer = 0
+    arr = []
+    for i in s:
+        if len(arr) == 0:
+            arr.append(i)
+        elif arr[-1] == i:
+            arr.pop()
+        else:
+            arr.append(i)
+    if len(arr) == 0:
+        answer = 1
+    return answer
